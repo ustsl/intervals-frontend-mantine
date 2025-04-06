c8070dac0eadf07f391fc290b34b1c31f49e41d2
ustsl <w@imvo.site>
2025-04-06 16:02:04 +0300

    upd

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     CRYPTO_KEY: process.env.CRYPTO_KEY,
-    AIHANDLER_SECRET_KEY: process.env.AIHANDLER_SECRET_KEY,
-    AIHANDLER_PROMPT: process.env.AIHANDLER_PROMPT
   },
 };
 
