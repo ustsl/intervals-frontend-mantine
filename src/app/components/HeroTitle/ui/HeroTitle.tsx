9063da5079c7552439e7ba4d8a4a786aae34c376
ustsl <w@imvo.site>
2025-04-14 18:19:30 +0300

    minor upd

--- src/app/components/HeroTitle/ui/HeroTitle.tsx
+++ src/app/components/HeroTitle/ui/HeroTitle.tsx
@@ -7,18 +7,20 @@ export const HeroTitle = () => {
     return (
         <div className={classes.wrapper}>
             <Container size={800} className={classes.inner}>
-                <Stack gap={'lg'}>
-                    <h1 className={classes.title} >
-                        Умные{' '}
-                        <Text component="span" variant="gradient" gradient={{ from: 'blue', to: 'cyan' }} inherit>
-                            дашборды для бизнеса
-                        </Text>
-                    </h1>
+                <Stack gap="lg">
+                    <Stack gap="sm">
+                        <h1 className={classes.title} >
+                            Умные{' '}
+                            <Text component="span" variant="gradient" gradient={{ from: 'blue', to: 'cyan' }} inherit>
+                                дашборды для бизнеса
+                            </Text>
+                        </h1>
 
-                    <Text className={classes.description} c="var(--mantine-color-gray-9)">
-                        Визуализируйте данные в дашборды.
-                        Создавайте сложные кастомизированные отчеты. Пишите интеграции под гибкий API сервиса.
-                    </Text>
+                        <Text className={classes.description} c="var(--mantine-color-gray-9)">
+                            Визуализируйте данные в дашборды.
+                            Создавайте сложные кастомизированные отчеты. Пишите интеграции под гибкий API сервиса.
+                        </Text>
+                    </Stack>
 
                     <Button
                         mt={'lg'}
